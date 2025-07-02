00d59aa380328d2caea9aead97fe4de073a3dd04
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2025-07-02 05:52:28 +0300

    Setup All App theme (Colors + Typography)

--- app/src/main/java/org/motager/storethemes/ui/theme/Color.kt
+++ app/src/main/java/org/motager/storethemes/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package org.motager.storethemes.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF000000)  // Primary
+val OnPrimary = Color(0xFFFFFFFF)  // OnPrimary
+val Secondary = Color(0xFF2E2E2E)  // Secondary
+val OnSecondary = Color(0xFF000000)  // OnSecondary
+val Background = Color(0xFFFFFFFF) // Background
+val OnBackground = Color(0xFF0B0A09) // OnBackground
+val Card = Color(0xFFF6F6F6)
+val CardForeground = Color(0xFF000000)
+val Outline = Color(0xFF9F9F9F) // Outline
+val Muted = Color(0xFFF5F5F5) // Tertiary
+val OnMuted = Color(0xFF989898) // OnTertiary
+val Input = Color(0xFF656565)  // SurfaceVariant
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+//val PopOverLight = Color(0xFFFFFFFF)
+//val PopOverForegroundLight = Color(0xFF0B0A09)
+//val PrimaryContainerLight = Color(0xAA7C2D12)  // PrimaryContainer
+//val OnPrimaryContainerLight = Color(0xFF292524)
+//
+//val AccentLight = Color(0xFFF5F5F0)
+//val AccentForeGroundLight = Color(0xFF2A251B)
+//val DestructiveLight = Color(0xFFE53935) // Error
+//val DestructiveForeGroundLight = Color(0xFFFCFDF6)
+//
+//val RingLight = Color(0xFFFF6F1A)
+//val Chart_1Light = Color(0xFF059669) // SurfaceContainerLow
+//val Chart_2Light = Color(0xFFD1FAE5)
